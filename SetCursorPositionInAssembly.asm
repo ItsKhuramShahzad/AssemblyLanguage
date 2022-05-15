d6dc26c8159747831787de5248b0583fc774db40
Khuram Shahzad <37767727+ItsKhuramShahzad@users.noreply.github.com>
2022-05-15 16:25:00 +0500

    Update SetCursorPositionInAssembly.asm

--- SetCursorPositionInAssembly.asm
+++ SetCursorPositionInAssembly.asm
@@ -3,19 +3,13 @@ org 100h
 jmp start
 
 setcursor:
-    mov  ax, 0003h    ; BIOS.SetVideoMode 80x25 16-color text
-    int  10h
-
-    mov  dx, 0C23h    ; DH is Row (12), DL is Column (35)
+    ;mov  dx, 0C23h    ; DH is Row (12), DL is Column (35)
+    mov dh, 20         ; Row number 12
+    mov dl , 35        ; Column No 35
     mov  bh, 0        ; DisplayPage
-    mov  ah, 02h      ; BIOS.SetCursorPosition
-    int  10h
-
-    mov  cx, 10       ; ReplicationCount
-    mov  bx, 002Fh    ; BH is DisplayPage (0) , BL is Attribute (BrightWhiteOnGreen)
-    mov  ax, 0941h    ; BIOS.WriteCharacterAndAttribute, AL is ASCII ("A")
-    int  10h
-
+    mov  ah, 02h      ; BIOS.SetCursorPosition (set cursor position function)
+    int  10h   ; interput to BIOS Display memory service to apply changes.
+    
     mov  ah, 00h      ; BIOS.WaitKeyboardKey
     int  16h          ; -> AX
 ret 
