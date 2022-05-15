7f6a78bd4491492cb0923c20edc791e4d20ade40
Khuram Shahzad <37767727+ItsKhuramShahzad@users.noreply.github.com>
2022-05-15 17:05:56 +0500

    Update ClearScreenUsingScrolldown.asm

--- ClearScreenUsingScrolldown.asm
+++ ClearScreenUsingScrolldown.asm
@@ -3,7 +3,7 @@ org 100h
 jmp start
 
 clearscreen:
-    MOV AH, 06h    ; Scroll down function
+    MOV AH, 07h    ; Scroll down function
     XOR AL, AL     ; number of lines by which to scroll up (00h = clear entire window)
     XOR CX, CX     ; Upper left corner CH=row, CL=column
     MOV DX, 184FH  ; lower right corner DH=row, DL=column 
